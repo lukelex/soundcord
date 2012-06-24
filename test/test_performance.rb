d665303cb128a79eb1d88848fdd64fc7832f4942
Lukas Alexandre <lukeskytm@gmail.com>
2012-06-24 00:14:14 -0300

    Adding performance test

---
+++ test/test_performance.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+require "benchmark"
+require 'test/unit'
+require 'soundcord'
+
+class SoundCordTest < Test::Unit::TestCase
+  def test_with_100_words
+    list_of_random_word = []
+    100.times do
+      list_of_random_word << (0...8).map{65.+(rand(25)).chr}.join
+    end
+    time = Benchmark.measure do
+      list_of_random_word.each { |i| i.phonetize }
+    end
+    assert_block do
+      time.real < 0.5
+    end
+  end
+end
