1b9be8a2212b057facc85f995acee56a19701bf3
Lukas Alexandre <lukeskytm@gmail.com>
2012-06-23 22:39:00 -0300

    Fix for ruby 1.8.7

--- lib/config.rb
+++ lib/config.rb
@@ -4,6 +4,8 @@ class SoundCord
   DEFAULT_LANGUAGE = 'pt-BR'
   LANGUAGES_DIRECTORY = "#{Dir.pwd}/lib/soundcord/languages/"
 
+  $KCODE = 'UTF8'
+
   def self.load_language lang = DEFAULT_LANGUAGE
     @language = lang
     @lang_yml = YAML::load_file(LANGUAGES_DIRECTORY + "#{lang}.yml")
--- test/test_soundcord.rb
+++ test/test_soundcord.rb
@@ -12,28 +12,29 @@ class SoundCordTest < Test::Unit::TestCase
     assert_equal "VLM", "Walmir".phonetize
   end
   def test_simple_comparations
-    # assert_equal true, "Joao".homophone?("João")
-    # assert_equal true, "Helena".homophone?("Elena")
-    # assert_equal true, "Walmir".homophone?("Valmir")
-    # assert_equal true, "Marria".homophone?("Maria")
-    # assert_equal true, "Wagner".homophone?("Vagner")
-    # assert_equal true, "Mirela".homophone?("Mirella")
-    # assert_equal true, "Artur".homophone?("Arthur")
-    # assert_equal true, "Diego".homophone?("Dyego")
-    # assert_equal true, "Felipe".homophone?("Phelipe")
-    # assert_equal true, "Filipe".homophone?("Felipe")
-    # assert_equal true, "Phelipe".homophone?("Filipe")
-    # assert_equal true, "Philippe".homophone?("Felipe")
+    assert_equal true, "Joao".homophone?("João")
+    assert_equal true, "Helena".homophone?("Elena")
+    assert_equal true, "Walmir".homophone?("Valmir")
+    assert_equal true, "Marria".homophone?("Maria")
+    assert_equal true, "Wagner".homophone?("Vagner")
+    assert_equal true, "Mirela".homophone?("Mirella")
+    assert_equal true, "Artur".homophone?("Arthur")
+    assert_equal true, "Diego".homophone?("Dyego")
+    assert_equal true, "Felipe".homophone?("Phelipe")
+    assert_equal true, "Filipe".homophone?("Felipe")
+    assert_equal true, "Phelipe".homophone?("Filipe")
+    assert_equal true, "Philippe".homophone?("Felipe")
+    assert_equal true, "Luçia".homophone?("lucia")
   end
   def test_use_vogals_option
-    # assert_equal "ELEMA", "Helena".phonetize(:use_vogals => true)
+    assert_equal "ELEMA", "Helena".phonetize(:use_vogals => true)
   end
   def test_find_in_collection
-    # list = %w( saola paulo saulo ricardo sallo )
-    # expected = %w( saola saulo sallo )
-    # assert_equal expected, list.homophones("saulo")
-    # list = %w( leonardo lucene rodrigo luciana lussene )
-    # expected = %w( lucene luciana lussene )
-    # assert_equal expected, list.homophones("lucene")
+    list = %w( saola paulo saulo ricardo sallo )
+    expected = %w( saola saulo sallo )
+    assert_equal expected, list.homophones("saulo")
+    list = %w( leonardo lucene rodrigo luciana lussene )
+    expected = %w( lucene luciana lussene )
+    assert_equal expected, list.homophones("lucene")
   end
 end
