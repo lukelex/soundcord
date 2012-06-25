a2331890e45b777ffd176b215af67264f6e984fe
Lukas Alexandre <lukeskytm@gmail.com>
2012-06-24 22:56:56 -0300

    Fixing config setters errors

--- lib/soundcord.rb
+++ lib/soundcord.rb
@@ -6,15 +6,15 @@ require 'algorithm'
 require 'config'
 
 class SoundCord
-  def self.phonetize text, options = { :use_vogals => false }
+  def self.phonetize text, options = { :use_vowels => false }
     handle_text(text, options)
   end
 
-  def self.compare term_1, term_2, options = { :use_vogals => false }
+  def self.compare term_1, term_2, options = { :use_vowels => false }
     homophone? term_1, term_2, options
   end
 
-  def self.homophone? term_1, term_2, options = { :use_vogals => false }
+  def self.homophone? term_1, term_2, options = { :use_vowels => false }
     phonetize(term_1, options) == phonetize(term_2, options)
   end
 end
