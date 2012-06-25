92abe46949aa4071b1d1ed7b64a055d98c72a625
Lukas Alexandre <lukeskytm@gmail.com>
2012-06-24 22:48:35 -0300

    Moving language specific tests to its folders

--- test/test_config.rb
+++ test/test_config.rb
@@ -8,4 +8,8 @@ class SoundCordTest < Test::Unit::TestCase
     SoundCord.load_language "pt-BR"
     assert_equal "pt-BR", SoundCord.language
   end
+  def test_language_set_up
+    SoundCord.load_language "en"
+    assert_equal "en", SoundCord.language
+  end
 end
