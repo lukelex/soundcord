a642fb5cfc5d7cdf68cddc43a395c2c14c17b698
Lukas Alexandre <lukeskytm@gmail.com>
2012-07-12 13:22:21 -0300

    Removing DEPRECATED methods

--- lib/soundcord/integrations/string.rb
+++ lib/soundcord/integrations/string.rb
@@ -13,17 +13,6 @@ class String
     value.phonetize
   end
 
-  # DEPRECATED: Please use homophone? instead.
-  def compare_phntc compared
-    warn "[DEPRECATION] `compare_phntc` is deprecated. Please use `homophone?` instead."
-    self.homophone? compared
-  end
-  # DEPRECATED: Please use homophone? instead.
-  def compare_phonetically compared
-    warn "[DEPRECATION] `compare_phonetically` is deprecated. Please use `homophone?` instead."
-    self.homophone? compared
-  end
-
   # Compares the passed value with the object value, both in their phonetic version
   # Params:
   # +use_vowels+:: enables the vowel comparison feature (if avaiable)
