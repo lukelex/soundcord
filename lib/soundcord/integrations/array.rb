188af076c1a53a823d37f833f3c26815faa1bc29
Lukas Alexandre <lukasalexandre@me.com>
2013-02-06 03:52:28 -0300

    Closes #4 Adding support form array collections

--- lib/soundcord/integrations/array.rb
+++ lib/soundcord/integrations/array.rb
@@ -2,7 +2,13 @@ class Array
   # Search possible homphone matches within the array object for a given string word
   # Params:
   # +value+:: string to be phonetized and compared with the array items
-  def homophones value
+  def homophones(value)
     self.select { |i| i.homophone? value }
+  end
+
+  def phonetize
+    self.map do |item|
+      item.to_s.phonetize
+    end
   end
 end
--- test/test_array.rb
+++ test/test_array.rb
@@ -6,5 +6,15 @@ require 'soundcord/integrations/array'
 class SoundCordTest < Test::Unit::TestCase
   def test_homophones_method_existance
     assert_equal Array.new.respond_to?(:homophones), true
+    assert_equal Array.new.respond_to?(:phonetize), true
+  end
+
+  def test_phonetizing_the_array
+    SoundCord.load_language 'en'
+
+    result = %w(Lukas Marcy Richard John).phonetize
+    expected = %w(LKS MRS RXRT JN)
+
+    assert_equal expected, result
   end
 end
